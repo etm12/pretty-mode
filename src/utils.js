e1b83c9175e322f4f13ceedf2e5b77d622c8e984
Stefan Rimaila <stefan@rimaila.fi>
2019-05-06 18:26:26 +0300

    feat(utils): Add takeUniqueWith helper, JSDoc

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,35 @@
+// @ts-check
 import * as K from 'kefir';
 import * as U from 'karet.util';
 import * as R from 'kefir.ramda';
 import * as L from 'kefir.partial.lenses';
 
+/**
+ * @template T
+ * @param {T} x
+ */
 export const not = x => !x;
 
+/**
+ * @template T
+ * @param {string} type
+ * @param {K.Observable<T, any>} source
+ */
 export const observeEvent = (type, source) => U.thru(
   source,
+  // @ts-ignore
   U.flatMapLatest(el => K.fromEvents(el, type).toProperty()),
 );
 
+/**
+ * @template T
+ * @param {K.Observable<T, any>} obs
+ */
 export const toProperty = obs => obs instanceof K.Observable
   ? obs.toProperty()
   : K.constant(obs);
 
+// @ts-ignore
 export const takeProps = ps => obs => U.thru(
   obs,
   U.view(L.props(...ps)),
@@ -21,4 +37,19 @@ export const takeProps = ps => obs => U.thru(
   U.mapValue(R.values),
 );
 
+/**
+ * @param {number} x
+ * @param {number} y
+ */
 export const xyToTranslate = (x, y) => U.string`translateX(${x}px) translateY(${y}px)`;
+
+export const movementDeltaFrom = U.mapValue(R.props(['movementX', 'movementY']));
+
+export const pageCoordsFrom = U.mapValue(R.props(['pageX', 'pageY']));
+
+// @ts-ignore
+export const takeUniqueWith = (fn, obs) => U.thru(
+  obs,
+  U.mapValue(fn),
+  U.skipDuplicates(R.identical),
+);
